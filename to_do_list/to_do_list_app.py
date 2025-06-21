d171cd05dd284febaffeb77a1e94f05f487729e9
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-06-21 19:26:51 +0530

    update

---
+++ to_do_list/to_do_list_app.py
@@ -0,0 +1,15 @@
+while True:
+    choice = int(input("-- TODO LIST MENU --\n1. View Tasks\n2. Add Tasks\n3. Delete Tasks\n4. Exit \nENTER ANY NUMBER : "))
+
+    if choice == 1:
+        #view task 
+        pass
+    elif choice == 2:
+        # add task
+        pass
+    elif choice == 3:
+        # delete task
+        pass
+    elif choice == 4:
+        # exit
+        break
