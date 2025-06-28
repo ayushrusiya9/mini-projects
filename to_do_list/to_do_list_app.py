03d19266ea3ba1924d8bfd950a32afd1a2cf6aaf
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-06-28 10:02:06 +0530

    update

--- to_do_list/to_do_list_app.py
+++ to_do_list/to_do_list_app.py
@@ -1,30 +1,53 @@
 while True:
-    choice = int(input("-- TODO LIST MENU --\n1. View Tasks\n2. Add Tasks\n3. Delete Tasks\n4. Exit \nENTER ANY NUMBER : "))
+    choice = int(input("-- TODO LIST MENU --\n1. Add Task\n2. View Tasks\n3. Delete Tasks\n4. Exit \nENTER ANY NUMBER : "))
 
     if choice == 1:
-        #view task
-        try:
-            with open('todo.txt','r') as file:
-                file.readline()
-                if not task :
-                    print("No task found!")
-                else:
-                    print("YOUR TODO LIST!")
-                    for i, task in enumerate(i, start=1):
-                        print(f'{i}.{task.strip()}')
-        except FileNotFoundError:
-            print(" 'todo.txt' file does not exist")
-
-    elif choice == 2:
         # add task
         task = input("ENTER TASK: ")
         with open('todo.txt', 'a') as file:
             file.write(task + '\n')
             print('Task added', task)
+       
+
+    elif choice == 2:
+        #view task
+        try:
+            with open('todo.txt','r') as file:
+                tasks = file.readlines()
+                if not tasks :
+                    print("No task found!")
+                else:
+                    print("YOUR TODO LIST TASKS!")
+                    for i, tasks in enumerate(tasks, start=1):
+                        print(f'{i}.{tasks.strip()}')
+        except FileNotFoundError:
+            print(" 'todo.txt' file does not exist")
+
     elif choice == 3:
         # delete task
-        pass
+        try:
+            with open('todo.txt','r') as file:
+                tasks = file.readlines()
+            if not tasks:
+                print("No task found!")
+            else:
+                print("YOUR TODO LIST TASKS!")
+                for i,task in enumerate(tasks, start=1):
+                    print(f"{i}. {task.strip()}")
+            
+            task_num = int(input("ENTER TASK NUMBER : "))
+
+            if 1 <= task_num <= len(tasks):
+                # tasks = list(tasks)
+                removed = tasks.pop(task_num - 1)
+                with open("todo.txt",'w') as file:
+                    file.writelines(tasks)
+                    print(f"task deleted: {removed.strip()}")
+            else:
+                print("INVALID NUMBER!!!!!")
+        except FileNotFoundError:
+            print("FILE NOT FOUND!!!!!!!!!")
+
     elif choice == 4:
         # exit
-        # pass
         break
